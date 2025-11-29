6871aef3db852fd60661b4b90c9dd39af5b52fa8
kvg-creator <kent.gudmundsen@kompetanseforbundet.no>
2025-11-29 13:22:00 +0100

    Update entrypoint.sh

--- owntone-server/entrypoint.sh
+++ owntone-server/entrypoint.sh
@@ -5,25 +5,30 @@ set -e
 mkdir -p /share/owntone/dbase_and_logs || true
 mkdir -p /share/owntone/music || true
 
-# Pek /var/cache/owntone til /share/owntone/dbase_and_logs (der databasen skal ligge)
+# Pek /var/cache/owntone til /share/owntone/dbase_and_logs (skrivbart område)
 rm -rf /var/cache/owntone
 ln -s /share/owntone/dbase_and_logs /var/cache/owntone || true
 
 # Sørg for at config-katalogen finnes
 mkdir -p /etc/owntone || true
 
-# Hvis vi ikke har noen config ennå, kopier eksempel-konfigen fra imaget
-if [ ! -f /etc/owntone/owntone.conf ] && [ -f /usr/share/doc/owntone/examples/owntone.conf ]; then
-  cp /usr/share/doc/owntone/examples/owntone.conf /etc/owntone/owntone.conf
+# Hvis vi ikke har noen config ennå, prøv å kopiere eksempel-konfig
+if [ ! -f /etc/owntone/owntone.conf ]; then
+  if [ -f /usr/share/doc/owntone/examples/owntone.conf ]; then
+    cp /usr/share/doc/owntone/examples/owntone.conf /etc/owntone/owntone.conf
+  elif [ -f /etc/owntone.conf ]; then
+    # fallback hvis imaget bruker /etc/owntone.conf som standard
+    cp /etc/owntone.conf /etc/owntone/owntone.conf
+  fi
 fi
 
-# Slå av IPv6 i config hvis fila finnes
+# Juster konfig hvis den finnes
 if [ -f /etc/owntone/owntone.conf ]; then
-  sed -i 's/^ipv6 *= *yes/ipv6 = no/' /etc/owntone/owntone.conf || true
+  # slå av IPv6 hvis den er på
+  sed -i 's/^ipv6 *=.*/ipv6 = no/' /etc/owntone/owntone.conf || true
+  # kjør som root inne i containeren (unngår feil "Could not lookup user owntone")
+  sed -i 's/^uid *=.*/uid = "root"/' /etc/owntone/owntone.conf || true
 fi
 
-# Gi eierskap til standard owntone-bruker (uid/gid 1000 i offisielt image)
-chown -R 1000:1000 /share/owntone || true
-
-# Start OwnTone i foreground med vår config
+# Start OwnTone i foreground med vår konfig
 exec owntone -f -c /etc/owntone/owntone.conf
