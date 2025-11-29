04832f7a203132aa2700a34661578a463c3245a8
kvg-creator <kent.gudmundsen@kompetanseforbundet.no>
2025-11-29 12:50:52 +0100

    Create entrypoint.sh

---
+++ owntone-server/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+# Sørg for at katalogene i /share finnes (mappes fra HA)
+mkdir -p /share/owntone/dbase_and_logs || true
+mkdir -p /share/owntone/music || true
+
+# Gi (for sikkerhets skyld) eierskap til UID/GID 1000, som Owntone-containeren bruker som standard
+chown -R 1000:1000 /share/owntone || true
+
+# Sørg for at /var/cache/owntone peker til /share/owntone/dbase_and_logs
+rm -rf /var/cache/owntone
+ln -s /share/owntone/dbase_and_logs /var/cache/owntone
+
+# (Valgfritt) slå av IPv6 i config hvis fila finnes
+if [ -f /etc/owntone.conf ]; then
+  sed -i 's/^ipv6 *= *yes/ipv6 = no/' /etc/owntone.conf || true
+fi
+
+# Start Owntone i foreground med default config
+exec owntone -f -c /etc/owntone.conf
